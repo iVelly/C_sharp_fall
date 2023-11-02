4734c43d6f15108b912ad99c8f9b03d3857279bb
iVelly <122292956+iVelly@users.noreply.github.com>
2023-11-02 08:39:10 -0400

    Lab_5_ Book_activity 11/2/2023

---
+++ Lab_5_Book/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace Lab_5_Book
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.txtPrice = new System.Windows.Forms.TextBox();
+            this.dtpDatePublished = new System.Windows.Forms.DateTimePicker();
+            this.lblFeedback = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 32.25F);
+            this.label1.Location = new System.Drawing.Point(88, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(214, 51);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Book Title";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 32.25F);
+            this.label2.Location = new System.Drawing.Point(88, 130);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(315, 51);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Date Published";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 32.25F);
+            this.label3.Location = new System.Drawing.Point(88, 222);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(121, 51);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Price";
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.Location = new System.Drawing.Point(308, 49);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(171, 20);
+            this.txtTitle.TabIndex = 3;
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.Location = new System.Drawing.Point(215, 239);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.Size = new System.Drawing.Size(171, 20);
+            this.txtPrice.TabIndex = 4;
+            // 
+            // dtpDatePublished
+            // 
+            this.dtpDatePublished.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpDatePublished.Location = new System.Drawing.Point(409, 145);
+            this.dtpDatePublished.Name = "dtpDatePublished";
+            this.dtpDatePublished.Size = new System.Drawing.Size(86, 20);
+            this.dtpDatePublished.TabIndex = 5;
+            // 
+            // lblFeedback
+            // 
+            this.lblFeedback.AutoSize = true;
+            this.lblFeedback.Font = new System.Drawing.Font("Microsoft Sans Serif", 24.25F);
+            this.lblFeedback.Location = new System.Drawing.Point(208, 387);
+            this.lblFeedback.Name = "lblFeedback";
+            this.lblFeedback.Size = new System.Drawing.Size(315, 38);
+            this.lblFeedback.TabIndex = 6;
+            this.lblFeedback.Text = "Feedback goes here";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(363, 344);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 7;
+            this.btnAdd.Text = "Add Book";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.lblFeedback);
+            this.Controls.Add(this.dtpDatePublished);
+            this.Controls.Add(this.txtPrice);
+            this.Controls.Add(this.txtTitle);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.TextBox txtPrice;
+        private System.Windows.Forms.DateTimePicker dtpDatePublished;
+        private System.Windows.Forms.Label lblFeedback;
+        private System.Windows.Forms.Button btnAdd;
+    }
+}
+
---
+++ Lab_5_Book/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab_5_Book
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            //Fill the text property of the Feedback variable with the book title, price, and date published with some descriptive text/strings
+            // the "value" property of a DateTimePicket returns variable. We converted it to a string in a append it to the string/text.
+            lblFeedback.Text = " Book Added: " + txtTitle.Text + "($" + txtPrice.Text + ") - Published " + dtpDatePublished.Value.ToString();
+        }
+    }
+}
